4c773e8f3b6653bccbfd2f68affc5ec1f76b106b
arul-manureva <kaarulmurugan.new@gmail.com>
2022-08-20 15:16:07 +0530

    first commit

--- App.js
+++ App.js
@@ -1,52 +1,114 @@
 import { useRef, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Renderer, TextureLoader } from "expo-three";
+import { THREE } from "expo-three";
+global.THREE = global.THREE || THREE;
+import * as React from "react";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
+import { Asset } from "expo-asset";
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
+// Create an Asset from a resource
+// const asset = Asset.fromModule(require("./image.png"));
 
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+// await asset.downloadAsync();
 
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    if (mesh && mesh.current) {
-      mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? "hotpink" : "orange"}
-      />
-    </mesh>
-  );
-}
+// This is the local URI
+// const uri = asset.localUri;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-    </View>
+    <GLView
+      style={{ flex: 1 }}
+      // onContextCreate={(gl) => {
+      //   // Create a WebGLRenderer without a DOM element
+      //   const renderer = new Renderer({ gl });
+      //   renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+      // }}
+      onContextCreate={(gl) => {
+        onContextCreate(gl).then(console.log).catch(console.error);
+      }}
+    />
   );
 }
+// function onContextCreate(gl) {
+//   gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+//   gl.clearColor(0, 1, 1, 1);
+
+//   // Create vertex shader (shape & position)
+//   const vert = gl.createShader(gl.VERTEX_SHADER);
+//   gl.shaderSource(
+//     vert,
+//     `
+//     void main(void) {
+//       gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+//       gl_PointSize = 150.0;
+//     }
+//   `
+//   );
+//   gl.compileShader(vert);
+
+//   // Create fragment shader (color)
+//   const frag = gl.createShader(gl.FRAGMENT_SHADER);
+//   gl.shaderSource(
+//     frag,
+//     `
+//     void main(void) {
+//       gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+//     }
+//   `
+//   );
+//   gl.compileShader(frag);
+
+//   // Link together into a program
+//   const program = gl.createProgram();
+//   gl.attachShader(program, vert);
+//   gl.attachShader(program, frag);
+//   gl.linkProgram(program);
+//   gl.useProgram(program);
+
+//   gl.clear(gl.COLOR_BUFFER_BIT);
+//   gl.drawArrays(gl.POINTS, 0, 1);
+
+//   gl.flush();
+//   gl.endFrameEXP();
+// }
+const onContextCreate = async (gl) => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    gl.drawingBufferWidth / gl.drawingBufferHeight,
+    0.1,
+    1000
+  );
+
+  const renderer = new Renderer({ gl });
+  renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+  renderer.setClearColor(0xffffff, 0);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    map: new TextureLoader().load(require("./assets/images/swmansion.png")),
+  });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 3;
+
+  const animate = () => {
+    this.rafID = requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.03;
+    cube.position.x = Math.sin(cube.rotation.x);
+    cube.position.y = Math.cos(cube.rotation.y);
+
+    renderer.render(scene, camera);
+
+    gl.endFrameEXP();
+  };
+  animate();
+};
 
 const styles = StyleSheet.create({
   container: {
